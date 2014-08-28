603105040b148c20f118ed298e80eef3f5baeb67
Matt Gold <mattbgold@gmail.com>
2014-08-28 08:23:02 -0400

    First Commit

---
+++ Crypto/CryptoExtensions.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Crypto
+{
+    public static class CryptoExtensions
+    {
+        public static byte[] ToBytes(this String str)
+        {
+            return ToBytes(str, null);
+        }
+        public static byte[] ToBytes(this String str, int? @base)
+        {
+            if (@base.HasValue) //This indicates the string is a *special* representation of bytes such as Hex or Base64
+            {
+                return Enumerable.Range(0, str.Length).Where(x => x % 2 == 0).Select(x => Convert.ToByte(str.Substring(x, 2), @base.Value)).ToArray();
+            }
+            else //we just want the byte representation of the string itself.
+            {
+                return System.Text.Encoding.ASCII.GetBytes(str);
+            }
+        }
+
+        public static byte[] XOR(this Byte[] self, IList<byte> key)
+        {
+            //Damn skippy, this xors a byte array with another byte array "key", repeating the key if needed.
+            return Enumerable.Range(0, self.Length).Select(i => (byte)(self[i] ^ key[i % key.Count()])).ToArray();
+        }
+
+        public static string ToHexString(this Byte[] self)
+        {
+            return BitConverter.ToString(self).Replace("-", "");
+        }
+    } 
+}
---
+++ Crypto/TextAnalysis.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Crypto
+{
+    public static class TextAnalysis
+    {
+        private static readonly Dictionary<string, int> bigramScores = new Dictionary<string, int>()
+	    {
+		    {"th", 152}, {"he", 128}, {"in", 94}, {"er", 94}, {"an", 82}, {"re", 68}, {"nd", 63},
+		    {"at", 59 }, {"on", 57},  {"nt", 56}, {"ha", 56}, {"es", 56}, {"st", 55}, {"en", 55},
+		    {"ed", 53},  {"to", 52},  {"it", 50}, {"ou", 50}, {"ea", 47}, {"hi", 46}, {"is", 46},
+		    {"or", 43},  {"ti", 34},  {"as", 33}, {"te", 27}, {"et", 19}, {"ng", 18}, {"of", 16},
+		    {"al", 9},   {"de", 9},   {"se", 8},  {"le", 8},  {"sa", 6},  {"si" ,5},  {"ar", 4}, 
+		    {"ve", 4},   {"ra", 4},   {"ld", 2},  {"ur", 2}
+	    };
+
+        public static string GetHighestScore(IEnumerable<string> candidates)
+        {
+            return candidates.OrderByDescending(c => GetScore(c)).First();
+        }
+
+        public static int GetScore(string str)
+        {
+            var spaceCount = str.Count(x => x == ' ');
+            str = str.Replace(" ", "").ToLower();
+
+            int score = spaceCount * 10;
+
+            for (int i = 0; i < str.Length - 1; i++)
+            {
+                var bigram = str[i].ToString() + str[i + 1].ToString();
+                if (bigramScores.ContainsKey(bigram))
+                {
+                    score += bigramScores[bigram];
+                }
+                else
+                {
+                    //TODO: expand here to subtract more based on really crappy chars
+                    score -= 1;
+                }
+            }
+            return score;
+        }
+    }
+}
