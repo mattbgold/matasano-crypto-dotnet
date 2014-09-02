ad4a6a61191b0493968cee9cb3c2a2600a2ab762
Matt Gold <mattbgold@gmail.com>
2014-09-02 13:52:29 -0400

    merged challenge8

---
+++ Crypto/BytesComparer.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Crypto
+{
+    class BytesComparer : IEqualityComparer<IEnumerable<byte>>
+    {
+
+        public bool Equals(IEnumerable<byte> x, IEnumerable<byte> y)
+        {
+            return x.SequenceEqual(y);
+        }
+
+        public int GetHashCode(IEnumerable<byte> obj)
+        {
+            return obj.Count();
+        }
+    }
+}
